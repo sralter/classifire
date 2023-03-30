a57178d83e601dc077eb6559ea1a90ddcb56ca3f
Samuel Alter <s.r.alter@icloud.com>
2023-03-30 11:51:08 -0400

    updated imageanalysis notebook and scratchpad, create streamlit folder

---
+++ 02_streamlit/alter_samuel_capstone_streamlit.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import joblib
+
+st.title('Classi**FIRE**')
+st.subheader('Predicting wildfires in the Santa Monica Mountains')
+st.subheader('Samuel Alter')
+st.subheader('BrainStation Data Science | Spring 2023')
+
