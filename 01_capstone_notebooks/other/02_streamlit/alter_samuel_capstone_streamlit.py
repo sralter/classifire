e37caa90471f8a4e93726ad6bdb0faae9d3da08b
Samuel Alter <s.r.alter@icloud.com>
2023-04-10 18:29:26 -0400

    cleaned up repo folders

--- 01_capstone_notebooks/other/02_streamlit/alter_samuel_capstone_streamlit.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-import joblib
-
-st.title('Classi**FIRE**')
-st.subheader('Predicting wildfires in the Santa Monica Mountains')
-st.subheader('Samuel Alter')
-st.subheader('BrainStation Data Science | Spring 2023')
-
